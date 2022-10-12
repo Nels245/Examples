1eb00665d5bede328bef6694bd767285a7267ae2
Nels <boldirevpavel97@yandex.ru>
2022-10-12 21:49:23 +0300

    add Example011 and Seminar_02 / 02_2

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,19 @@
+﻿
+//              0   1   2  3  4 5 6  7 8  - индексы массива
+int[] array = {133,211,32,43,54,6,72,43,91};
+
+int n = array.Length; // длина массива
+int find = 43;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //поиск только первого элемента, там два элемента со значением 43
+    }
+    //index = index + 1
+    index++;
+}
---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,53 @@
+﻿//Заполнение массива рандомными числами
+
+    void FillArray(int[] collection)
+        {
+            int length =collection.Length;
+            int index = 0;
+            while (index < length)
+            {
+                collection[index] = new Random().Next(1,10);
+                index++;
+            }
+        }
+
+// Функция вывода массива
+      
+    void PrintArray(int[] col)
+        {
+            int count = col.Length;
+            int position = 0;
+            while (position <count)
+            {
+                Console.WriteLine(col[position]);
+                position++;
+            }
+        }
+        
+// Фукнция первого вхождения искомого индекса
+
+    int IndexOf(int[] collection, int find)
+    {
+        int count = collection.Length;
+        int index = 0;
+        int position = -1; // Задаем позицию -1, т.к если нужного елемента не будет в массиве он вывел -
+        while (index < count)
+        {
+            if(collection[index] == find)
+            {
+                position = index;
+                break;
+            }
+            index++;
+        }
+        return position;
+    }
+        
+    int [] array = new int[10];
+    
+    FillArray(array); // Заполнение массива
+    PrintArray(array); // Вывод массива в консоль
+    Console.WriteLine(); // Обычный отступ)
+    
+    int pos = IndexOf(array, 4); // Поиск первого вхождения элемента 4 в массиве array
+    Console.WriteLine(pos);
